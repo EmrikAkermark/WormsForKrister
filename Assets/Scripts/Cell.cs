a750920ac1367afaf48f52324504e7771f99b874
Emrik Åkermark <headcrab.farmer@gmail.com>
2020-12-27 18:25:09 +0100

    Made project

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,27 @@
+﻿public class Cell
+{
+    public enum CellContents
+    {
+        Empty,
+        Wall,
+        Snake,
+        Food
+    }
+
+    public CellContents CellContent;
+
+    public Cell(CellContents startContent)
+    {
+        CellContent = startContent;
+    }
+
+    public CellContents GetContents()
+    {
+        return CellContent;
+    }
+
+    public void ChangeContent(CellContents newContent)
+    {
+        CellContent = newContent;
+    }
+}
---
+++ Assets/Scripts/CellGrid.cs
@@ -0,0 +1,22 @@
+﻿public class CellGrid
+{
+    public int Width, Height;
+
+    public Cell[,] Grid;
+
+    private int minWidht = 4, minHeight = 4;
+
+    public CellGrid(int Width, int Height)
+    {
+        if(Width < minWidht)
+        {
+            Width = minWidht;
+        }
+        if(Height < minHeight)
+        {
+            Height = minHeight;
+        }
+        this.Width = Width;
+        this.Height = Height;
+    }
+}
